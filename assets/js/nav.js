b9dc45a415f1f5962b6b138fb2eaa3010f9de010
Larry Mayers <larry.mayers@outlook.com>
2020-11-19 12:11:22 -0400

    Form validation features updated

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -96,18 +96,186 @@ class Nav{
               /* Collect enter data from LEVEL dropdown selection field. */         
            this.usrSessTime = document.getElementById("usrSessTime");
            this.sessErrorMsg = document.getElementById("sessErrorMsg");
-          
-            this.stuDef = document.getElementsByName("stuDef");
-            
-           
+           /* Collect entered data from user FEEDBACK field. */         
            this.usrMsg = document.getElementById("feedbackMsg");
            this.feedbackErrorMsg = document.getElementById("feedbackErrorMsg");
-           this.msgValid = false;
+          
+           this.stuDef = document.getElementsByName("stuDef");
+
            this.emailConfirm = document.getElementById("emailConfirm");
-           
-           this.topicValid = false;
+            
            this.btnSubmit = document.getElementById("btnSubmit");
-           this.emailValid = false;
+            
+            this.emailValid = false;
+            this.topicValid = false;
+            this.msgValid = false;
+
+          /* Function to respond to user interaction with NAME input field  */
+            this.nameInput.addEventListener("blur", () => {
+                 /* USER NAME Input Check */
+        if(!Validator.validate(this.nameInput.value, Validator.REQUIRED)){
+            this.nameErrorMsg.innerText = "Blank feedback NOT allowed!!!";
+            this.nameInput.style = "border-color: #ff0000;";
+            this.nameErrorMsg.style = "color: #ff0000;";
+
+            return;
+            
+        }else if(!Validator.validate(this.nameInput.value, Validator.MAX_LENGTH, 25)){
+            this.nameErrorMsg.innerText = "Feedback exceeds limit!";
+            this.nameInput.style = "border-color: #ff0000;";
+            this.nameErrorMsg.style = "color: #ff0000;";
+        
+            return;
+        
+        }else if(Validator.validate(this.nameInput.value, Validator.REQUIRED)){
+        
+            this.nameErrorMsg.innerText = "";
+            this.nameInput.style = "border-color: #2ecc71;";
+            this.nameErrorMsg.style = "display: none";
+        
+            this.msgValid = true;
+        }                
+
+            });
+
+            /* Function to respond to user interaction with CONTACT input field  */
+            this.phoneInput.addEventListener("blur", () => {
+                /* USER NAME Input Check */
+       if(!Validator.validate(this.phoneInput.value, Validator.REQUIRED)){
+           this.phoneErrorMsg.innerText = "Blank feedback NOT allowed!!!";
+           this.phoneInput.style = "border-color: #ff0000;";
+           this.phoneErrorMsg.style = "color: #ff0000;";
+
+           return;
+           
+       }else if(!Validator.validate(this.phoneInput.value, Validator.MAX_LENGTH, 18)){
+           this.phoneErrorMsg.innerText = "Feedback exceeds limit!";
+           this.phoneInput.style = "border-color: #ff0000;";
+           this.phoneErrorMsg.style = "color: #ff0000;";
+       
+           return;
+       
+       }else if(Validator.validate(this.phoneInput.value, Validator.REQUIRED)){
+       
+           this.phoneErrorMsg.innerText = "";
+           this.phoneInput.style = "border-color: #2ecc71;";
+           this.phoneErrorMsg.style = "display: none";
+       
+           this.msgValid = true;
+       }                
+
+           });
+
+        /* Function to respond to user interaction with EMAIL input field  */
+            this.emailInput.addEventListener("blur", () => {
+
+            /* EMAIL Check */
+        if(!Validator.validate(this.emailInput.value, Validator.REQUIRED)){
+            this.emailErrorMsg.innerText = "Missing Email Info!";
+            this.emailInput.style = "border-color: #ff0000;";
+            this.emailErrorMsg.style = "color: #ff0000;";
+
+            return;
+
+        }else if(!Validator.validate(this.emailInput.value, Validator.MAX_LENGTH, 35)){
+                    this.emailErrorMsg.innerText = "Feedback exceeds limit!";
+                    this.emailInput.style = "border-color: #ff0000;";
+                    this.emailErrorMsg.style = "color: #ff0000;";
+                
+                    return;
+        
+        } else if(!Validator.validate(this.emailInput.value, Validator.EMAIL)){
+            this.emailErrorMsg.innerText = "Email format incorrect!";
+            this.emailInput.style = "border-color: #ff0000;";
+            this.emailErrorMsg.style = "color: #ff0000;";
+
+            return;
+
+        } else if(Validator.validate(this.emailInput.value, Validator.EMAIL)) {
+
+            this.emailErrorMsg.innerText = "";
+            this.emailInput.style = "border-color: #2ecc71;";
+            this.emailErrorMsg.style = "display: none";
+
+            this.emailValid = true;
+        }
+
+            });
+
+              /* Function to respond to user interaction with PHONE input field  */
+              this.phoneInput.addEventListener("blur", () => {
+
+
+            });
+
+    /* Function to respond to user interaction with this input field  */
+           this.usrTopic.addEventListener("blur", () => {
+
+        /* USER TOPIC SELECTION Check */
+        if(!Validator.validate(this.usrTopic.value, Validator.DROP_BOX)){
+            this.topicErrorMsg.innerText = "Please select a feedback topic!";
+            this.usrTopic.style = "border-color: #ff0000;";
+            this.topicErrorMsg.style = "color: #ff0000;";
+        
+            return;
+            
+        } else if (Validator.validate(this.usrTopic.value, Validator.DROP_BOX)){
+        
+            this.topicErrorMsg.innerText = "";
+            this.usrTopic.style = "border-color: #2ecc71;";
+            this.topicErrorMsg.style = "display: none";
+        
+            this.topicValid = true;
+        }
+         
+        });
+
+        
+       /* Function to respond to user interaction with this input field  */
+        this.usrLevel.addEventListener("blur", () => {
+            
+         /* MESSAGE Check */
+         if(!Validator.validate(this.usrLevel.value, Validator.DROP_BOX)){
+            this.levelErrorMsg.innerText = "Blank feedback NOT allowed!!!";
+            this.usrLevel.style = "border-color: #ff0000;";
+            this.levelErrorMsg.style = "color: #ff0000;";
+        
+            return;
+            
+        } else if(Validator.validate(this.usrLevel.value, Validator.DROP_BOX)){
+        
+            this.levelErrorMsg.innerText = "";
+            this.usrLevel.style = "border-color: #2ecc71;";
+            this.levelErrorMsg.style = "display: none";
+        
+            this.msgValid = true;
+        }
+         
+        
+        });
+        
+         /* Function to respond to user interaction with this input field  */
+         this.usrSessTime.addEventListener("blur", () => {
+            
+            /* MESSAGE Check */
+            if(!Validator.validate(this.usrSessTime.value, Validator.DROP_BOX)){
+               this.sessErrorMsg.innerText = "Blank feedback NOT allowed!!!";
+               this.usrSessTime.style = "border-color: #ff0000;";
+               this.sessErrorMsg.style = "color: #ff0000;";
+           
+               return;
+               
+           } else if(Validator.validate(this.usrSessTime.value, Validator.DROP_BOX)){
+           
+               this.sessErrorMsg.innerText = "";
+               this.usrSessTime.style = "border-color: #2ecc71;";
+               this.sessErrorMsg.style = "display: none";
+           
+               this.msgValid = true;
+           }
+            
+           
+           });
 
         }
 
@@ -143,6 +311,8 @@ static validate(value, flag, validatorValue){
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
     }
 }
+
+
 
 
 }
