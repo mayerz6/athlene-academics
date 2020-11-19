d4636a05b05bd75638d4d0c792657ea7364a091b
Larry Mayers <larry.mayers@outlook.com>
2020-11-18 22:07:28 -0400

    Revisions to the contact form JS functionality

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,42 +1,41 @@
 /* Primary site navigation functionality */
 
-/* OBJECT definition */
+/* Primary link navigation OBJECT definition */
 class Nav{
 
     constructor(){
                 /* XML Object Definition */
                this.xhr = new XMLHttpRequest();
            
+               /* ABOUT link definition */
                this.aboutLink = document.getElementById("about");
+               /* CONTACT link definition */
                this.contactLink = document.getElementById("contact");
-            
+           
+               /* EVENTLISTENERS defined to respond to user interaction */
                this.aboutLink.addEventListener("click", this.clickAboutHandler);
                this.contactLink.addEventListener("click", this.clickContactHandler);  
            
+               /* Copyright YEAR Definition */
                window.addEventListener("load", () => {
                 document.getElementById("year").appendChild(document.createTextNode(new Date().getFullYear()));
                });
        }      
       
       
-   
+/* Function defined to load info ASYNCHRONOUSLY from the server*/
       clickAboutHandler(){ 
    
-           console.log(`The ABOUT link was clicked!!!`);  
-          // window.location.href = "./about.html";
-        
           let xhr = new XMLHttpRequest();
         
           xhr.open('GET', './assets/regions/content/about.html', true);
        
           xhr.onreadystatechange = function(){
-              if(this.readyState == 4 && this.status == 200){
-               document.getElementById("content").innerHTML = this.responseText;
-      
+          if(this.readyState == 4 && this.status == 200) {
+               document.getElementById("content").innerHTML = this.responseText; 
             }
           }
-      
-          xhr.onerror = function(){
+                  xhr.onerror = function(){
               console.log("Data request error...");
           }
       
@@ -44,25 +43,21 @@ class Nav{
        
        }
    
-   
+  /* Function defined to load info ASYNCHRONOUSLY from the server */
        clickContactHandler(){  
-           console.log("The CONTACT link was clicked!");  
-           
-           // window.location.href = "./about.html";
-        
-              let xhr = new XMLHttpRequest();
+      
+        let xhr = new XMLHttpRequest();
         
               xhr.open('GET', './assets/regions/content/contact.html', true);
            
               xhr.onreadystatechange = function(){
                   if(this.readyState == 4 && this.status == 200){
-                   document.getElementById("content").innerHTML = this.responseText;
-          
-                  // let block = document.getElementById("content");
-                  //   block.innerHTML = this.responseText;
-                     //console.clear()
-                     //  console.log(this.responseText);
-                  }
+                  
+                    document.getElementById("content").innerHTML = this.responseText;
+                            new Form();
+                    /* Include contact form functionality here. */
+                  
+                      }
               }
           
               xhr.onerror = function(){
@@ -75,6 +70,83 @@ class Nav{
           
    }
    
+
+   class Form{
+   
+    constructor(){
+    
+            /* Collect enter data from NAME input field. */         
+           this.nameInput = document.getElementById("nameInput");
+           this.nameErrorMsg = document.getElementById("nameErrorMsg");
+           /* Collect enter data from EMAIL input field. */         
+           this.emailInput = document.getElementById("emailInput");
+           this.emailErrorMsg = document.getElementById("emailErrorMsg");
+          /* Collect enter data from CONTACT input field. */         
+           this.phoneInput = document.getElementById("phoneInput");
+           this.phoneErrorMsg = document.getElementById("phoneErrorMsg");
+          /* Collect enter data from TOPIC dropdown selection field. */         
+           this.usrTopic = document.getElementById("usrTopic");
+           this.topicErrorMsg = document.getElementById("topicErrorMsg");
+           /* Collect enter data from LEVEL dropdown selection field. */         
+           this.usrLevel = document.getElementById("usrLevel");
+           this.levelErrorMsg = document.getElementById("levelErrorMsg");
+            /* Collect enter data from LEVEL dropdown selection field. */         
+           this.usrSessTime = document.getElementById("usrSessTime");
+           this.sessErrorMsg = document.getElementById("sessErrorMsg");
+              /* Collect enter data from LEVEL dropdown selection field. */         
+           this.usrSessTime = document.getElementById("usrSessTime");
+           this.sessErrorMsg = document.getElementById("sessErrorMsg");
+          
+            this.stuDef = document.getElementsByName("stuDef");
+            
+           
+           this.usrMsg = document.getElementById("feedbackMsg");
+           this.feedbackErrorMsg = document.getElementById("feedbackErrorMsg");
+           this.msgValid = false;
+           this.emailConfirm = document.getElementById("emailConfirm");
+           
+           this.topicValid = false;
+           this.btnSubmit = document.getElementById("btnSubmit");
+           this.emailValid = false;
+
+        }
+
+   }
+
+   /* Designation of the CLASS used to validate our user input. */
+class Validator {
+
+    static REQUIRED = "REQUIRED";
+    static MIN_LENGTH = "MIN_LENGTH";
+    static NUMBER = "NUMBER";
+    static MAX_LENGTH = "MAX_LENGTH";
+    static DROP_BOX = "DROP_BOX";
+    static EMAIL = "EMAIL";
+
+static validate(value, flag, validatorValue){
+    if(flag === this.REQUIRED){
+        return value.trim().length > 0;
+    }
+    if(flag === this.MIN_LENGTH){
+        return value.trim().length > validatorValue;
+    }
+    if(flag === this.MAX_LENGTH){
+        return value.length < validatorValue;
+    }
+    if(flag === this.NUMBER){
+        return isNaN(value) ;
+    }
+    if(flag === this.DROP_BOX){
+        return value > 0;
+    }
+    if(flag === this.EMAIL){
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+    }
+}
+
+
+}
+
    new Nav();
    
    
